aeb1704f8ab1ef04efb22f842d0dc7db70375f44
Sahil Singh <sahilgusain100@gmail.com>
2026-09-20 16:07:48 +0530

    all setup config

--- BackendProject/src/app.js
+++ BackendProject/src/app.js
@@ -14,7 +14,6 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
 //routes import
 import userRouter from "./routes/user.routes.js"
 
--- BackendProject/src/controllers/user.controller.js
+++ BackendProject/src/controllers/user.controller.js
@@ -2,8 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (requestAnimationFrame, res) => {
+
       res.status(200).json({
-        messgae: "Backend Project"
+        message: "Backend Project"
     })
 } )
 
